6d7db14c3a7424ed9128d7153ccece3ed9d2c7d7
v-kalc <59079664+v-kalc@users.noreply.github.com>
2020-09-24 18:26:51 +0530

    Updated 1

--- Source/Microsoft.Teams.Apps.NewHireOnboarding/BackgroundService/LearningPlanNotification.cs
+++ Source/Microsoft.Teams.Apps.NewHireOnboarding/BackgroundService/LearningPlanNotification.cs
@@ -5,7 +5,6 @@
 namespace Microsoft.Teams.Apps.NewHireOnboarding.BackgroundService
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Net;
     using System.Threading;
